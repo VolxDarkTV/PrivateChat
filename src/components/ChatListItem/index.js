bcca626513a8361a480db0a81fe69c7dadccb927
Vincenzo <vincenzo.foti@outlook.it>
2023-08-19 01:14:17 +0200

    ChatScreen static PrivateChat

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,4 +1,7 @@
 import {StyleSheet, View, Text, Image } from 'react-native';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
 
 const ChatListItem = ({ chat }) => {
 
@@ -9,7 +12,7 @@ const ChatListItem = ({ chat }) => {
             <View style={styles.content}>
                 <View style={styles.row}>
                     <Text numberOfLines={1} style={styles.name}>{chat.user.name}</Text>
-                    <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
+                    <Text style={styles.subTitle}>{dayjs(chat.lastMessage.createdAt).fromNow(true)}</Text>
                 </View>
 
                 <Text numberOfLines={2} style={styles.subTitle}>{chat.lastMessage.text}</Text>
