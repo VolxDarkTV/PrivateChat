143ead7ae86b3edd5da4bc037940bd230b064bee
Vincenzo <vincenzo.foti@outlook.it>
2023-08-22 19:45:22 +0200

    delete users deleted = true

--- src/components/screens/ContactsScreen.js
+++ src/components/screens/ContactsScreen.js
@@ -1,24 +1,47 @@
 import { useState, useEffect } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, Button } from "react-native";
 import ContactListItem from "../ContactListItem";
 import { API, graphqlOperation } from "aws-amplify";
 import { listUsers } from "../../graphql/queries";
+import { deleteUser } from "../../graphql/mutations";
 
 const ContactsScreen = () => {
-  //TAKE DATA API & QUERY
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
-    API.graphql(graphqlOperation(listUsers)).then((result) => {
-      setUsers(result.data?.listUsers?.items)
-    })
-  }, [])
+    const fetchUsers = async () => {
+      const response = await API.graphql(graphqlOperation(listUsers));
+      const userList = response.data.listUsers.items;
+      const filteredUsers = userList.filter(user => user._deleted !== true);
+      setUsers(filteredUsers);
+      // console.log(filteredUsers.length);
+    };
+    fetchUsers();
+  }, []);
+
+  
+  const deleteMarkedUsers = async () => {
+    for (const user of users) {
+      if (user._deleted === true) {
+        await API.graphql(graphqlOperation(deleteUser, { input: { id: user.id } }));
+      }
+    }
+    setUsers(users.filter((user) => user._deleted !== true));
+    console.log(users.filter((user) => user._deleted !== true));
+  };
 
   return (
-    <FlatList
-      data={users}
-      renderItem={({ item }) => <ContactListItem user={item} />}
-      style={{ backgroundColor: "white" }}
-    />
+    <View>
+      <FlatList
+        data={users}
+        renderItem={({ item }) => <ContactListItem user={item} />}
+        style={{ backgroundColor: "white" }}
+      />
+      <Button
+        title="Delete Marked Users"
+        onPress={deleteMarkedUsers}
+      />
+    </View>
   );
 };
 
